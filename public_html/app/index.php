5146b29a6de35221a0159ff7bea43e2ce24c1c1c
Chris Gregg <chris@greggs.org>
2022-03-11 14:19:39 +0000

    add some spice

--- public_html/app/index.php
+++ public_html/app/index.php
@@ -1,3 +1,10 @@
 <?php
+    require_once '/users/init.php';  //make sure this path is correct!
+    require_once $abs_us_root.$us_url_root.'users/includes/template/prep.php';
+    if (!securePage($_SERVER['PHP_SELF'])){die();}
+?>
+
+
+<?php
 echo "Hello";
 ?>
